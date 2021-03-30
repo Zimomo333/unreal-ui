39b0a7f99fbac21a1f86a8a98a236c40aaee4621
Zimomo333 <lzh120908717@gmail.com>
2021-03-30 20:10:12 +0800

    update：tag padding

--- packages/tag/tag.vue
+++ packages/tag/tag.vue
@@ -59,7 +59,7 @@ export default {
     box-shadow: inset 2px 2px 5px #b8b9be, inset -3px -3px 7px #FFFFFF;
     display: inline-block;
     height: 1.4rem;
-    padding: 0 .3rem 0 .6rem;
+    padding: 0 .6rem;
     font-size: 0.7rem;
     font-weight: bold;
     text-align: center;
